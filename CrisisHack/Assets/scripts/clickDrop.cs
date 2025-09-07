482af18faaa883fe08a8f42000b2cb9099230325
Pr0grameer <alexzlam@outlook.com>
2025-09-07 13:58:42 -0700

    Final Push

--- CrisisHack/Assets/scripts/clickDrop.cs
+++ CrisisHack/Assets/scripts/clickDrop.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+
 
 public class ClickAndPlaceUIImage : MonoBehaviour
 {
     public GameObject selectedImg0;
-    //public GameObject selectedImg1;
-    //public GameObject selectedImg2;
-    //public GameObject selectedImg3;
-    //public GameObject selectedImg4;
+    public GameObject selectedImg1;
+    public GameObject selectedImg2;
+    public GameObject selectedImg3;
+    public GameObject selectedImg4;
+    public GameObject selectedImg5;
+    public GameObject selectedImg6;
+    public GameObject selectedImg7;
+    public GameObject selectedImg8;
+
+    public GameObject Chat;
+    
 
     bool imgSelected = false;
     GameObject selectedImg;
@@ -21,6 +28,7 @@ public class ClickAndPlaceUIImage : MonoBehaviour
 
     public void Update()
     {
+        
         bool click = Input.GetMouseButtonDown(0);
         if (click == true && imgSelected == true) // Left mouse button clicked
         {
@@ -30,12 +38,48 @@ public class ClickAndPlaceUIImage : MonoBehaviour
             Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
             
             mouseWorldPos.z = 0f;
-            
+        
             Instantiate(selectedImg, mouseWorldPos, Quaternion.identity);
             imgSelected = false;
         }
+        
     }
-    public void clickDrop0()
+    public void clickDrop(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                imgSelected = true;
+                selectedImg = selectedImg0; break;
+            case 1:
+                imgSelected = true;
+                selectedImg = selectedImg1;  break;
+            case 2:
+                imgSelected = true;
+                selectedImg = selectedImg2 ; break;
+            case 3:
+                imgSelected = true;
+                selectedImg = selectedImg3; break;
+            case 4:
+                imgSelected = true;
+                selectedImg = selectedImg4; break;
+            case 5:
+                imgSelected = true;
+                selectedImg = selectedImg5; break;
+            case 6:
+                imgSelected = true;
+                selectedImg = selectedImg6; break;
+            case 7:
+                imgSelected = true;
+                selectedImg = selectedImg7; break;
+            case 8:
+                imgSelected = true;
+                selectedImg = selectedImg8; break;
+        }
+
+    }
+
+    /*public void clickDrop0()
     {
         Debug.Log("Hellow");
         imgSelected = true;
@@ -66,6 +110,37 @@ public class ClickAndPlaceUIImage : MonoBehaviour
         imgSelected = true;
 
     }
+    public void clickDrop5()
+    {
+        Debug.Log("Hellow");
+        imgSelected = true;
+        selectedImg = selectedImg0;
+
+    }
+    public void clickDrop6()
+    {
+        Debug.Log("Hellow");
+        imgSelected = true;
+
+    }
+    public void clickDrop7()
+    {
+        Debug.Log("Hellow");
+        imgSelected = true;
+
+    }
+    public void clickDrop8()
+    {
+        Debug.Log("Hellow");
+        imgSelected = true;
+
+    }
+    public void clickDrop9()
+    {
+        Debug.Log("Hellow");
+        imgSelected = true;
+
+    }*/
 
 
 }
